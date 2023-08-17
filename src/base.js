fe4cdb2a227eb3804ad486cf814b1d8783face72
Bilal Ahmed <bilalahmedim@gmail.com>
2023-08-17 23:28:31 +0530

    toggle working

--- src/base.js
+++ src/base.js
@@ -1,5 +1,5 @@
 const toggleBtn = document.querySelector('.hb');
-const nav = document.querySelector('.side-drawer');
+const nav = document.querySelector('.nav-nav');
 
 toggleBtn.addEventListener('click', () => {
   nav.classList.toggle('open');
