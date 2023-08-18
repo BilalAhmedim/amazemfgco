22ad80c9d6d329d4482e31a2e25458bfb1be3f04
Bilal Ahmed <bilalahmedim@gmail.com>
2023-08-18 21:49:56 +0530

    toggle nav

--- src/base.js
+++ src/base.js
@@ -1,6 +1,14 @@
 const toggleBtn = document.querySelector('.hb');
 const nav = document.querySelector('.nav-nav');
+const model = document.querySelector('.model');
 
 toggleBtn.addEventListener('click', () => {
   nav.classList.toggle('open');
+  model.classList.toggle('open');
+});
+
+
+model.addEventListener("click", () => {
+  nav.classList.toggle('open');
+  model.classList.toggle('open');
 });
