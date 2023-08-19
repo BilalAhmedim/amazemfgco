6b013f44bea234ba916151dadb787765cf9bf9ef
Bilal Ahmed <bilalahmedim@gmail.com>
2023-08-19 21:26:22 +0530

    dropdown navigation working animation left.

--- src/base.js
+++ src/base.js
@@ -12,3 +12,23 @@ model.addEventListener("click", () => {
   nav.classList.toggle('open');
   model.classList.toggle('open');
 });
+
+
+// navigation sub menu 
+const parentItems = document.querySelectorAll('.has-submenu');
+
+parentItems.forEach(item => {
+  const sub_menu = item.nextElementSibling;
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+    closeAllSubmenus();
+    sub_menu.classList.toggle('open');
+  });
+});
+
+function closeAllSubmenus() {
+  const openSubmenus = document.querySelectorAll('.sub-menu.open');
+  openSubmenus.forEach(submenu => {
+    submenu.classList.remove('open');
+  });
+}
