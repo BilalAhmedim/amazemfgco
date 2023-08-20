33912997b1479be024b4c687693dbc84119f965c
Bilal Ahmed <bilalahmedim@gmail.com>
2023-08-20 19:20:34 +0530

    toggle buttun bug fixedby simulating click

--- src/base.js
+++ src/base.js
@@ -7,10 +7,20 @@ toggleBtn.addEventListener('click', () => {
   model.classList.toggle('open');
 });
 
-
+// Model
 model.addEventListener("click", () => {
   nav.classList.toggle('open');
   model.classList.toggle('open');
+
+  // when we click on model its simulate click on toggle svg
+  const rectElement = document.querySelector('.hb rect');
+  if (rectElement) {
+    const animateElement = rectElement.querySelector('animate');
+
+    if (animateElement) {
+      animateElement.beginElement();
+    }
+  }
 });
 
 
